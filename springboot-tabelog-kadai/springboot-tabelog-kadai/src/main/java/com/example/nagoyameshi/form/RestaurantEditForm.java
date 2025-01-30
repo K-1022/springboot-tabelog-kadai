3c8a96fb101ec43e88c33295cbfcbba5eab8b66b
K-1022 <167005898+K-1022@users.noreply.github.com>
2025-01-30 18:49:49 +0900

    Add files via upload

--- springboot-tabelog-kadai/springboot-tabelog-kadai/src/main/java/com/example/nagoyameshi/form/RestaurantEditForm.java
+++ springboot-tabelog-kadai/springboot-tabelog-kadai/src/main/java/com/example/nagoyameshi/form/RestaurantEditForm.java
@@ -11,9 +11,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
  
  @Data
  @AllArgsConstructor
+ @NoArgsConstructor
  public class RestaurantEditForm {
      @NotBlank(message = "店舗名を入力してください。")
      private String name;
